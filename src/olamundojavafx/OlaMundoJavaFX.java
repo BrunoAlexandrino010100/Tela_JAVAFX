f5fd5fa6193d29cf658fe47d54480c666e06a65f
Bruno Alexandrino <bruno-alexandrino@hotmail.com>
2022-09-09 15:12:19 -0300

    Tela em JAVA FX, exibe uma mensagem com evento clique

---
+++ src/olamundojavafx/OlaMundoJavaFX.java
@@ -0,0 +1,36 @@
+//Programa utilizando o JAVA FX.
+//Apresenta uma janela com um botão.
+//Quando a ação de clique, a mensagem "Olá Mundo", é apresentada.
+
+package olamundojavafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Bruno Alexandrino
+ */
+public class OlaMundoJavaFX extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
